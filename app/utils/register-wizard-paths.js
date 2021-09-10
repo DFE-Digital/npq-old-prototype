55512fa7546f9790dba8d86c9466c7644eae69f4
Paul Hayes <fofr@users.noreply.github.com>
2021-09-10 12:52:08 +0100

    Add dont know and not got TRN pages

--- app/utils/register-wizard-paths.js
+++ app/utils/register-wizard-paths.js
@@ -12,7 +12,10 @@ function registerWizardPaths (req) {
     '/register/confirmation',
 
     '/register/chosen',
-    '/register/choosing-an-npq'
+    '/register/choosing-an-npq',
+
+    '/register/trn',
+    '/register/get-your-trn'
   ]
 
   return nextAndBackPaths(paths, req)
@@ -25,6 +28,19 @@ function registerWizardForks (req) {
       storedData: ['register', 'chosen'],
       values: ['no'],
       forkPath: '/register/choosing-an-npq'
+    },
+    {
+      currentPath: '/register/trn',
+      storedData: ['register', 'know-trn'],
+      values: ['dont-know', 'no-trn'],
+      forkPath: (value) => {
+        switch (value) {
+          case 'dont-know':
+            return '/register/get-your-trn'
+          case 'no-trn':
+            return '/register/get-a-trn'
+        }
+      }
     }
   ]
   return nextForkPath(forks, req)
