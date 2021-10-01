8af8b6b7b4f4874c83dc12e4810371a27e865f90
Paul Hayes <fofr@users.noreply.github.com>
2021-10-01 11:17:02 +0100

    Give structure to feature flags

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,7 +1,19 @@
 module.exports = {
   features: {
-    'aso-loop': false,
-    'international-and-non-teacher': true,
-    'name-changes': true
+    'aso-loop': {
+      on: false,
+      name: 'ASO Loop',
+      description: 'Allow users to loop from the confirmation page back into an ASO course registration journey'
+    },
+    'international-and-non-teacher': {
+      on: true,
+      name: 'International and Non-teacher journeys',
+      description: 'Tailor registration journey for international teachers and non-teachers'
+    },
+    'name-changes': {
+      on: true,
+      name: 'Name changes',
+      description: 'Use the original ‘Name changes’ journey, which asks about whether someone has changed their name since they became a teacher'
+    }
   }
 }
