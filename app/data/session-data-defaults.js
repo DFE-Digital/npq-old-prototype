e94151527103fb8a168ce42d28c858865caf8f17
Paul Hayes <fofr@users.noreply.github.com>
2022-01-11 15:43:32 +0000

    Disable registration closed flag by default

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -7,7 +7,7 @@ module.exports = {
       description: 'Allow users to loop from the confirmation page back into an ASO course registration journey'
     },
     'registration-closed': {
-      on: true,
+      on: false,
       name: 'Registration closed',
       description: 'How the service behaves when you cannot register for Autumn 2022 yet'
     }
