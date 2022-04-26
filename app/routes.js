88f77494ba5455a2af5cc9373e74c459a8ca05c3
Paul Hayes <fofr@users.noreply.github.com>
2022-04-26 12:29:53 +0100

    Receive data from account flow

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,11 @@ require('./routes/register')(router)
 require('./routes/soft-close')(router)
 require('./routes/accounts')(router)
 
+router.post('/receive-data', (req, res) => {
+  req.session.data['account-data'] = JSON.parse(req.body['account-data'])
+  res.redirect('/register/where-do-you-work')
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
